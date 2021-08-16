fec40aa9938ba325ae1986bb049a9377a1a10507
DanA <Danny13653@gmail.com>
2021-08-16 20:02:30 +0100

    Updated GetSubmissions

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,22 @@
  *  Make use of Args inside other API calls.
  *  Only Export API-MAIN not other Classes. */
 
+import { LogAPI } from './Log';
+
 export { DiscordConfigAPI } from './DiscordConfig';
 export { RedditConfigAPI } from './RedditConfig';
 export { TicketAPI } from './TicketConfig';
 export { LogAPI } from './Log';
 
 //Testing
-// (async () => {})();
+// (async () => {
+//   const logAPI = new LogAPI();
+//   const data = await logAPI.getSubmissionIds(
+//     'a19050a0-adff-464f-9c10-401e5ff601cb'
+//   );
+
+//   console.log(data);
+// })();
 
 export {
   GuildConfigDto,

1e1021be46118b77663eb55cb0134aa94d6519ed
DanA <Danny13653@gmail.com>
2021-08-16 01:32:05 +0100

    Forgot to Export Reddit Class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/* Add Main Class API-MAIN which takes options as an argument
+ *  These Options can be URL, TOKEN, ETC.
+ *  Make use of Args inside other API calls.
+ *  Only Export API-MAIN not other Classes. */
+
 export { DiscordConfigAPI } from './DiscordConfig';
+export { RedditConfigAPI } from './RedditConfig';
 export { TicketAPI } from './TicketConfig';
 export { GuildConfigDto, TicketDto, PaymentDto } from './types';
